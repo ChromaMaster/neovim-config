4c6850b3140243b53bb4ec68c4bf3d6f14bcbabe
ChromaMaster <vlad@frescocooks.com>
2023-09-19 15:21:20 +0200

    feat: update lsp-zero to v3

--- lua/chromamaster/lazy.lua
+++ lua/chromamaster/lazy.lua
@@ -1,50 +1,59 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- Fuzzy finder
-	{ 'nvim-telescope/telescope.nvim', tag = '0.1.3', dependencies = { 'nvim-lua/plenary.nvim' } },
+    -- Fuzzy finder
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.3', dependencies = { 'nvim-lua/plenary.nvim' } },
 
-	-- Theme 
-	{ 'rose-pine/neovim', name = 'rose-pine' },
+    -- Theme 
+    { 'rose-pine/neovim', name = 'rose-pine' },
 
-	-- Syntax highlighting
-	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    -- Syntax highlighting
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 
-	-- Jump between opened files
-	{ 'ThePrimeagen/harpoon', dependencies = { 'nvim-lua/plenary.nvim' } },
+    -- Jump between opened files
+    { 'ThePrimeagen/harpoon', dependencies = { 'nvim-lua/plenary.nvim' } },
 
-	-- See full history of changes
-	{ 'mbbill/undotree' }, 
+    -- See full history of changes
+    { 'mbbill/undotree' }, 
 
-	-- Use git inside nvim
-	{ 'tpope/vim-fugitive' },
+    -- Use git inside nvim
+    { 'tpope/vim-fugitive' },
+   
+    -- All this is for the lsp
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    {'williamboman/mason.nvim'},
+    
+    {'williamboman/mason-lspconfig.nvim'},
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	},
+    -- LSP Support
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+        },
+    },
+
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'},
+        }
+    },
+    -- until here
+
 
     { 'github/copilot.vim' }
 })
