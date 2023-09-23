a7a8867e6ee4abad1f84d5ca80bca43711b0fc99
ChromaMaster <vlad@frescocooks.com>
2023-09-23 21:03:21 +0200

    feat(lsp): install lsp servers automatically using mason-lspconfig

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,22 +6,28 @@ lsp.on_attach(function(client, bufnr)
 	lsp.default_keymaps({buffer = bufnr})
 end)
 
-lsp.setup_servers({
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {
+    'lua_ls', -- Lua
     'cmake', -- Cmake
     'bashls', -- Bash
     'clangd', -- C/C++
     'jedi_language_server', -- Python
 	'rust_analyzer', -- Rust
 	'gopls', -- Go
+  },
+  handlers = {
+    lsp.default_setup,
+  }
 })
 
 lsp.setup()
 
 -- The plugin responsible for autocompletion is nvim-cmp
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup()
-
-
 
